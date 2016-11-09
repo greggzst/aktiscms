eee585a3eb19677bd958cfdf561208851cd0aeca
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2016-11-09 21:14:32 +0100

    Edit Section view and Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :section_elements
-  resources :sections
+  resources :sections do
+  	resources :section_elements
+  end
   root 'sections#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

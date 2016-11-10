de75695f4a4a9f206b49b5c4aa49a9294b6c7597
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2016-11-10 14:48:12 +0100

    Add section and section elements connection in form

--- app/controllers/section_elements_controller.rb
+++ app/controllers/section_elements_controller.rb
@@ -3,29 +3,22 @@ class SectionElementsController < ApplicationController
   before_action :get_sections, only: [:new, :create, :edit, :update]
   before_action :set_section_element, only: [:show, :edit, :update, :destroy]
 
-  # GET /section_elements
-  # GET /section_elements.json
   def index
     @section_elements = SectionElement.all
   end
 
-  # GET /section_elements/1
-  # GET /section_elements/1.json
   def show
   end
 
-  # GET /section_elements/1/edit
   def edit
   end
 
-  # POST /section_elements
-  # POST /section_elements.json
   def create
     @section_element = @section.section_elements.create(section_element_params)
 
     respond_to do |format|
       if @section_element.save
-        format.html { redirect_to @section_element, notice: 'Section element was successfully created.' }
+        format.html { redirect_to @section, notice: 'Section element was successfully created.' }
         format.json { render :show, status: :created, location: @section_element }
       else
         format.html { render :new }
@@ -34,12 +27,10 @@ class SectionElementsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /section_elements/1
-  # PATCH/PUT /section_elements/1.json
   def update
     respond_to do |format|
       if @section_element.update(section_element_params)
-        format.html { redirect_to @section_element, notice: 'Section element was successfully updated.' }
+        format.html { redirect_to @section, notice: 'Section element was successfully updated.' }
         format.json { render :show, status: :ok, location: @section_element }
       else
         format.html { render :edit }
@@ -48,8 +39,6 @@ class SectionElementsController < ApplicationController
     end
   end
 
-  # DELETE /section_elements/1
-  # DELETE /section_elements/1.json
   def destroy
     @section_element.destroy
     respond_to do |format|
@@ -61,7 +50,7 @@ class SectionElementsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_section_element
-      @section_element = SectionElement.find(params[:id])
+      @section_element = @section.section_elements.find(params[:id])
     end
 
     def get_sections
--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,28 +1,20 @@
 class SectionsController < ApplicationController
   before_action :set_section, only: [:show, :edit, :update, :destroy]
 
-  # GET /sections
-  # GET /sections.json
   def index
     @sections = Section.all
   end
 
-  # GET /sections/1
-  # GET /sections/1.json
   def show
   end
 
-  # GET /sections/new
   def new
     @section = Section.new
   end
 
-  # GET /sections/1/edit
   def edit
   end
 
-  # POST /sections
-  # POST /sections.json
   def create
     @section = Section.new(section_params)
 
@@ -37,8 +29,6 @@ class SectionsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /sections/1
-  # PATCH/PUT /sections/1.json
   def update
     respond_to do |format|
       if @section.update(section_params)
@@ -51,8 +41,6 @@ class SectionsController < ApplicationController
     end
   end
 
-  # DELETE /sections/1
-  # DELETE /sections/1.json
   def destroy
     @section.destroy
     respond_to do |format|

9a12d650c62c2b05a20dea1214d03b0927683614
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2016-11-12 21:42:44 +0100

    Add Link

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :links
   resources :sections do
   	get 'change_visibility', on: :member
   	resources :section_elements do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161109125941) do
+ActiveRecord::Schema.define(version: 20161112203925) do
+
+  create_table "links", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "section_elements", force: :cascade do |t|
     t.string   "title"
